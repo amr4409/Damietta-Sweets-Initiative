3a775cad6c4d55b410e490db8f31e7a81353c58f
amr4409 <87361692+amr4409@users.noreply.github.com>
2022-03-18 09:15:45 +0200

    Add files via upload

---
+++ الشخصيات/test/javascript/js.js
@@ -0,0 +1,7 @@
+// Start Header
+let btnMenuHeader = document.getElementById("iconListHeaderMenu");
+let headerMenu = document.getElementById("headerMenu");
+btnMenuHeader.onclick = function () {
+  headerMenu.classList.toggle("active");
+};
+// End Header 
